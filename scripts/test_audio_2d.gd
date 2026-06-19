8f71390c78a98cf862b280206477cc016864bdff
Qhuimin <125970640+Qhuimin@users.noreply.github.com>
2026-06-19 14:07:37 +0800

    Revert "Merge branch 'main' into huimin"
    This reverts commit f4b6ede3971f3dcfd9d98613c67520985c8105ec, reversing
    changes made to 90ccf5fb5bd08226fd103c1e5525ec7d78fd9382.

--- scripts/test_audio_2d.gd
+++ scripts/test_audio_2d.gd
@@ -4,4 +4,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	AudioManager.play_sound(0, self);
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
