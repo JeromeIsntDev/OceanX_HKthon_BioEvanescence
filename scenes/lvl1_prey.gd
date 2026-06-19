d243882b253da90a086f4bf69c35b45639c549ee
Mohamed Khilfi Bin Mohamed Nazarudin <94026490+KhilfiNazarudin@users.noreply.github.com>
2026-06-19 14:12:24 +0800

    Corrected Orientation

--- scenes/lvl1_prey.gd
+++ scenes/lvl1_prey.gd
@@ -25,7 +25,7 @@ func _physics_process(delta: float) -> void:
 		
 		State.SWIM:
 			velocity = swimDirection * SPEED
-			sprite.flip_v = swimDirection.x < 0  # flip if moving left
+			sprite.flip_h = swimDirection.x > 0  # flip if moving left
 			
 			if stateDuration >= SWIM_DURATION:
 				change_state(State.IDLE)
